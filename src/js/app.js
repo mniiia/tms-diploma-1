2bcb0fb96a8150b53a312922ef6a13ca1749239f
unknown <lolmisha76@gmail.com>
2024-03-11 18:42:03 +0300

    inint (diploma):added main functional

--- src/js/app.js
+++ src/js/app.js
@@ -3,17 +3,64 @@ import * as bootstrap from 'bootstrap';
 const modalWindowElement = document.querySelector('.modal');
 const todoFormElement = document.querySelector('#todoForm');
 const cardContainer = document.querySelector('.card__container');
+const cardContainerInProgress = document.querySelector('.card__container__inprogress');
+const cardContainerDone = document.querySelector('.card__container__done');
+const todoCounter = document.querySelector('.trello-todo-counter');
+const inProgressCounter = document.querySelector('.trello-inprogress-counter');
+const doneCounter = document.querySelector('.trello-done-counter');
+const trelloContainerElement = document.querySelector('.trello__container');
+
+//time
+const timeElement = document.querySelector('.trello__time');
+timeElement.textContent = getTime();
+
+function getTime(date = new Date()) {
+  let seconds = date.getSeconds();
+  if (seconds < 10) {
+    seconds = `0${seconds}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  return `${hours}:${minutes}`;
+}
+
+setInterval(() => (timeElement.textContent = getTime()), 1000);
 
 const todos = [];
+const inprogress = [];
+const done = [];
 
 //проверка содержимого LS
 if (localStorage.getItem('todoList')) {
   const dataFromLS = JSON.parse(localStorage.getItem('todoList'));
   for (let i = 0; i < dataFromLS.length; i++) {
     todos[i] = dataFromLS[i];
-    console.log(todos[i]);
+    // console.log(todos[i]);
   }
   renderTodo();
+}
+
+if (localStorage.getItem('inprogressList')) {
+  const dataFromLS = JSON.parse(localStorage.getItem('inprogressList'));
+  for (let i = 0; i < dataFromLS.length; i++) {
+    inprogress[i] = dataFromLS[i];
+  }
+  renderTodoInProgress();
+}
+
+if (localStorage.getItem('doneList')) {
+  const dataFromLS = JSON.parse(localStorage.getItem('doneList'));
+  for (let i = 0; i < dataFromLS.length; i++) {
+    done[i] = dataFromLS[i];
+  }
+  renderTodoDone();
 }
 
 //добавление todo после нажатия кнопки confirm
@@ -34,19 +81,37 @@ function addTodo() {
     description: document.getElementById('description').value,
     user: document.querySelector('.select-add-users').value,
     time: date,
+    whichPanel: 'todo',
   };
-  console.log(document.querySelector('.select-add-users').value);
+  // console.log(document.querySelector('.select-add-users').value);
 
   document.getElementById('title').value = '';
   document.getElementById('description').value = '';
   todos.push(todoObj);
-  saveDataToLocalStorage(todos);
+  saveDataToLocalStorage('todoList', todos);
   renderTodo();
 }
 
 //функция создает шаблон todo
 function createTemplate(todoList) {
   const time = new Date(todoList.time);
+  let select1;
+  let select2;
+  let select3;
+  switch (todoList.whichPanel) {
+    case 'todo':
+      select1 = 'selected';
+      break;
+    case 'inprogress':
+      select2 = 'selected';
+      break;
+    case 'done':
+      select3 = 'selected';
+      break;
+
+    default:
+      break;
+  }
   return `
     <div class="card" id="${todoList.id}">
         <div class="card-body">
@@ -54,7 +119,7 @@ function createTemplate(todoList) {
             <p class="card-text">${todoList.description}</p>
             <div class="card-bottom">
                 <p class="card-user">${todoList.user}</p>
-                <p class="card-time">${time.getHours()}:${time.getMinutes()}</p>
+                <p class="card-time">${getTime(time)}</p>
             </div>
             <div class="card-management">
                 <button
@@ -65,10 +130,10 @@ function createTemplate(todoList) {
                 >
                 <a href="#" class="btn card-btn-edit edit-todo"><i class="fa-solid fa-pen edit-todo"></i></a>
                 </button>
-                <select class="form-select" aria-label="Disabled select example">
-                <option selected value="1">todo</option>
-                <option value="2">in progress</option>
-                <option value="3">done</option>
+                <select id="selectPanel" class="form-select" aria-label="Disabled select example">
+                <option ${select1} value="todo">todo</option>
+                <option ${select2} value="inprogress">in progress</option>
+                <option ${select3} value="done">done</option>
                 </select>
                 <button class="card-management-button delete-task-button">
                 <a id="deleteOneTodoButton" href="#" class="btn card-btn-delete"><i id="deleteOneTodoIcon" class="fa-solid fa-trash"></i></a>
@@ -84,37 +149,80 @@ function createTemplate(todoList) {
 function renderTodo() {
   let html = '';
   for (let i = 0; i < todos.length; i++) {
+    todos[i].whichPanel = 'todo';
     html += createTemplate(todos[i]);
   }
   cardContainer.innerHTML = html;
+  todoCounter.innerHTML = todos.length;
+}
+
+function renderTodoInProgress() {
+  let html = '';
+  for (let i = 0; i < inprogress.length; i++) {
+    inprogress[i].whichPanel = 'inprogress';
+    html += createTemplate(inprogress[i]);
+  }
+  cardContainerInProgress.innerHTML = html;
+  inProgressCounter.innerHTML = inprogress.length;
+}
+
+function renderTodoDone() {
+  let html = '';
+  for (let i = 0; i < done.length; i++) {
+    done[i].whichPanel = 'done';
+    html += createTemplate(done[i]);
+  }
+  cardContainerDone.innerHTML = html;
+  doneCounter.innerHTML = done.length;
 }
 
 //тут все понятно
-function saveDataToLocalStorage(todoList) {
-  console.log(JSON.stringify(todoList));
-  localStorage.setItem('todoList', JSON.stringify(todoList));
+function saveDataToLocalStorage(key, todoList) {
+  // console.log(JSON.stringify(todoList));
+  localStorage.setItem(`${key}`, JSON.stringify(todoList));
 }
 
 //функция удаляет одно todo
 
-cardContainer.addEventListener('click', deleteOneTodo);
+trelloContainerElement.addEventListener('click', deleteOneTodo);
 
 function deleteOneTodo(event) {
+  console.log(event.target.id, event.target.id === 'deleteOneTodoIcon', '--------------');
   if (
     event.target.id === 'deleteOneTodoButton' ||
     event.target.id === 'deleteOneTodoIcon' ||
-    event.target.classList.contains('card-btn-delete')
+    event.target.classList.contains('delete-task-button')
   ) {
     const cardId = event.target.closest('.card').id;
-    console.log(cardId);
+    // console.log(cardId);
     for (let i = 0; i < todos.length; i++) {
-      console.log(typeof cardId, typeof todos[i].id);
+      // console.log(typeof cardId, typeof todos[i].id);
       if (cardId === `${todos[i].id}`) {
-        console.log(todos);
+        // console.log(todos);
         const slicedTodos = todos.splice(i, 1);
-        console.log(todos);
-        saveDataToLocalStorage(todos);
+        // console.log(todos);
+        saveDataToLocalStorage('todoList', todos);
         renderTodo();
+      }
+    }
+    for (let i = 0; i < inprogress.length; i++) {
+      // console.log(typeof cardId, typeof todos[i].id);
+      if (cardId === `${inprogress[i].id}`) {
+        // console.log(todos);
+        const slicedTodos = inprogress.splice(i, 1);
+        // console.log(todos);
+        saveDataToLocalStorage('inprogressList', inprogress);
+        renderTodoInProgress();
+      }
+    }
+    for (let i = 0; i < done.length; i++) {
+      // console.log(typeof cardId, typeof todos[i].id);
+      if (cardId === `${done[i].id}`) {
+        // console.log(todos);
+        const slicedTodos = done.splice(i, 1);
+        // console.log(todos);
+        saveDataToLocalStorage('doneList', done);
+        renderTodoDone();
       }
     }
   }
@@ -124,19 +232,33 @@ function deleteOneTodo(event) {
 
 const editFormElement = document.querySelector('#todoFormEdit');
 
-cardContainer.addEventListener('click', handleClickEditTodoForm);
+trelloContainerElement.addEventListener('click', handleClickEditTodoForm);
 let saveId;
 
 function handleClickEditTodoForm(event) {
   if (event.target.classList.contains('edit-todo')) {
     const cardId = event.target.closest('.card').id;
     saveId = cardId;
-    console.log(saveId, '----------------');
+    // console.log(saveId, '----------------');
     for (let i = 0; i < todos.length; i++) {
       if (cardId === `${todos[i].id}`) {
         document.getElementById('titleEdit').value = todos[i].title;
         document.getElementById('descriptionEdit').value = todos[i].description;
         document.querySelector('.select-edit-users').value = todos[i].user;
+      }
+    }
+    for (let i = 0; i < inprogress.length; i++) {
+      if (cardId === `${inprogress[i].id}`) {
+        document.getElementById('titleEdit').value = inprogress[i].title;
+        document.getElementById('descriptionEdit').value = inprogress[i].description;
+        document.querySelector('.select-edit-users').value = inprogress[i].user;
+      }
+    }
+    for (let i = 0; i < done.length; i++) {
+      if (cardId === `${done[i].id}`) {
+        document.getElementById('titleEdit').value = done[i].title;
+        document.getElementById('descriptionEdit').value = done[i].description;
+        document.querySelector('.select-edit-users').value = done[i].user;
       }
     }
   }
@@ -151,8 +273,26 @@ function editTodo(event) {
         todos[i].title = document.getElementById('titleEdit').value;
         todos[i].description = document.getElementById('descriptionEdit').value;
         todos[i].user = document.querySelector('.select-edit-users').value;
-        saveDataToLocalStorage(todos);
+        saveDataToLocalStorage('todoList', todos);
         renderTodo();
+      }
+    }
+    for (let i = 0; i < inprogress.length; i++) {
+      if (saveId === `${inprogress[i].id}`) {
+        inprogress[i].title = document.getElementById('titleEdit').value;
+        inprogress[i].description = document.getElementById('descriptionEdit').value;
+        inprogress[i].user = document.querySelector('.select-edit-users').value;
+        saveDataToLocalStorage('inprogressList', inprogress);
+        renderTodoInProgress();
+      }
+    }
+    for (let i = 0; i < done.length; i++) {
+      if (saveId === `${done[i].id}`) {
+        done[i].title = document.getElementById('titleEdit').value;
+        done[i].description = document.getElementById('descriptionEdit').value;
+        done[i].user = document.querySelector('.select-edit-users').value;
+        saveDataToLocalStorage('doneList', done);
+        renderTodoDone();
       }
     }
   }
@@ -163,23 +303,22 @@ function editTodo(event) {
 async function foo() {
   const users = await fetch('https://jsonplaceholder.typicode.com/users');
   const usersList = await users.json();
-  console.log(usersList);
+  // console.log(usersList);
   addUsers(usersList);
   editUser(usersList);
 }
-
 foo();
 
 const selectElementAddTodo = document.querySelector('.select-add-users');
 const selectElementEditTodo = document.querySelector('.select-edit-users');
-console.log(selectElementEditTodo);
+// console.log(selectElementEditTodo);
 
 function addUsers(usersList) {
   for (let i = 0; i < usersList.length; i++) {
     const newUser = document.createElement('option');
     newUser.value = usersList[i].username;
     newUser.innerHTML = usersList[i].username;
-    console.log(newUser);
+    // console.log(newUser);
     selectElementAddTodo.append(newUser);
   }
 }
@@ -189,7 +328,85 @@ function editUser(usersList) {
     const newUser = document.createElement('option');
     newUser.value = usersList[i].username;
     newUser.innerHTML = usersList[i].username;
-    console.log(newUser);
+    // console.log(newUser);
     selectElementEditTodo.append(newUser);
+  }
+}
+
+//перемещение карточек
+
+trelloContainerElement.addEventListener('click', handleClickChangePanel);
+
+function handleClickChangePanel(event) {
+  if (event.target.id === 'selectPanel') {
+    if (event.target.value === 'inprogress') {
+      const cardId = event.target.closest('.card').id;
+      for (let i = 0; i < todos.length; i++) {
+        if (`${todos[i].id}` === cardId) {
+          inprogress.push(todos[i]);
+          todos.splice(i, 1);
+          saveDataToLocalStorage('todoList', todos);
+          saveDataToLocalStorage('inprogressList', inprogress);
+          renderTodo();
+          renderTodoInProgress();
+        }
+      }
+      for (let i = 0; i < done.length; i++) {
+        if (`${done[i].id}` === cardId) {
+          inprogress.push(done[i]);
+          done.splice(i, 1);
+          saveDataToLocalStorage('doneList', done);
+          saveDataToLocalStorage('inprogressList', inprogress);
+          renderTodoInProgress();
+          renderTodoDone();
+        }
+      }
+    }
+    if (event.target.value === 'todo') {
+      const cardId = event.target.closest('.card').id;
+      for (let i = 0; i < inprogress.length; i++) {
+        if (`${inprogress[i].id}` === cardId) {
+          todos.push(inprogress[i]);
+          inprogress.splice(i, 1);
+          saveDataToLocalStorage('todoList', todos);
+          saveDataToLocalStorage('inprogressList', inprogress);
+          renderTodo();
+          renderTodoInProgress();
+        }
+      }
+      for (let i = 0; i < done.length; i++) {
+        if (`${done[i].id}` === cardId) {
+          todos.push(done[i]);
+          done.splice(i, 1);
+          saveDataToLocalStorage('todoList', todos);
+          saveDataToLocalStorage('doneList', done);
+          renderTodo();
+          renderTodoDone();
+        }
+      }
+    }
+    if (event.target.value === 'done') {
+      const cardId = event.target.closest('.card').id;
+      for (let i = 0; i < inprogress.length; i++) {
+        if (`${inprogress[i].id}` === cardId) {
+          done.push(inprogress[i]);
+          inprogress.splice(i, 1);
+          saveDataToLocalStorage('doneList', done);
+          saveDataToLocalStorage('inprogressList', inprogress);
+          renderTodoInProgress();
+          renderTodoDone();
+        }
+      }
+      for (let i = 0; i < todos.length; i++) {
+        if (`${todos[i].id}` === cardId) {
+          done.push(todos[i]);
+          todos.splice(i, 1);
+          saveDataToLocalStorage('todoList', todos);
+          saveDataToLocalStorage('doneList', done);
+          renderTodo();
+          renderTodoDone();
+        }
+      }
+    }
   }
 }
