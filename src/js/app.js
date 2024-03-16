3268b3e0c7de3141f0ecf423fdf231c7b3d65516
unknown <lolmisha76@gmail.com>
2024-03-16 23:49:08 +0300

    tried to apply modules

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,20 @@
+import {
+  handleClickTodoForm,
+  deleteOneTodo,
+  handleClickEditTodoForm,
+  handleClickEditTodo,
+  handleClickChangePanel,
+  handleClickDeleteAll,
+} from './handlers.js';
 import * as bootstrap from 'bootstrap';
 
 const modalWindowElement = document.querySelector('.modal');
 const todoFormElement = document.querySelector('#todoForm');
-const cardContainer = document.querySelector('.trello__card-container');
-const cardContainerInProgress = document.querySelector('.trello__card-container-inprogress');
-const cardContainerDone = document.querySelector('.trello__card-container-done');
+export const cardContainer = document.querySelector('.trello__card-container');
+export const cardContainerInProgress = document.querySelector('.trello__card-container-inprogress');
+export const cardContainerDone = document.querySelector('.trello__card-container-done');
 const todoCounter = document.querySelector('.trello__todo-counter');
-const inProgressCounter = document.querySelector('.trello__inprogress-counter');
+export const inProgressCounter = document.querySelector('.trello__inprogress-counter');
 const doneCounter = document.querySelector('.trello__done-counter');
 const trelloContainerElement = document.querySelector('.trello__container');
 
@@ -34,19 +42,18 @@ function getTime(date = new Date()) {
 setInterval(() => (timeElement.textContent = getTime()), 1000);
 
 //колонки
-const todos = [];
-const inprogress = [];
-const done = [];
+export const todos = [];
+export const inprogress = [];
+export const done = [];
 
-const progressionTabs = [todos, inprogress, done];
-const localStorageKeys = ['todoList', 'inprogressList', 'doneList'];
+export const progressionTabs = [todos, inprogress, done];
+export const localStorageKeys = ['todoList', 'inprogressList', 'doneList'];
 
 //проверка содержимого LS
 if (localStorage.getItem('todoList')) {
   const dataFromLS = JSON.parse(localStorage.getItem('todoList'));
   for (let i = 0; i < dataFromLS.length; i++) {
     todos[i] = dataFromLS[i];
-    // console.log(todos[i]);
   }
   renderTodo();
 }
@@ -69,14 +76,14 @@ if (localStorage.getItem('doneList')) {
 
 //добавление todo после нажатия кнопки confirm
 todoFormElement.addEventListener('click', handleClickTodoForm);
-function handleClickTodoForm(event) {
-  if (event.target.id === 'confirm') {
-    addTodo();
-  }
-}
+// function handleClickTodoForm(event) {
+//   if (event.target.id === 'confirm') {
+//     addTodo();
+//   }
+// }
 
 //функция добавляющая данные в todo и в LC
-function addTodo() {
+export function addTodo() {
   const date = new Date();
   class todoObj2 {
     id = Date.now();
@@ -156,13 +163,13 @@ function createTemplate({id, createdAt, whichPanel, title, description, user}) {
 }
 
 //функции рендера
-function renderAll() {
+export function renderAll() {
   renderTodo();
   renderTodoDone();
   renderTodoInProgress();
 }
 
-function renderTodo() {
+export function renderTodo() {
   let html = '';
   for (let i = 0; i < todos.length; i++) {
     todos[i].whichPanel = 'todo';
@@ -171,7 +178,7 @@ function renderTodo() {
   cardContainer.innerHTML = html;
   todoCounter.innerHTML = todos.length;
 }
-function renderTodoInProgress() {
+export function renderTodoInProgress() {
   let html = '';
   for (let i = 0; i < inprogress.length; i++) {
     inprogress[i].whichPanel = 'inprogress';
@@ -180,7 +187,7 @@ function renderTodoInProgress() {
   cardContainerInProgress.innerHTML = html;
   inProgressCounter.innerHTML = inprogress.length;
 }
-function renderTodoDone() {
+export function renderTodoDone() {
   let html = '';
   for (let i = 0; i < done.length; i++) {
     done[i].whichPanel = 'done';
@@ -191,12 +198,11 @@ function renderTodoDone() {
 }
 
 //тут все понятно
-function saveDataToLocalStorage(key, todoList) {
-  // console.log(JSON.stringify(todoList));
+export function saveDataToLocalStorage(key, todoList) {
   localStorage.setItem(`${key}`, JSON.stringify(todoList));
 }
 
-function saveAllDataToLocalStorage() {
+export function saveAllDataToLocalStorage() {
   for (let i = 0; i < progressionTabs.length; i++) {
     localStorage.setItem(`${localStorageKeys[i]}`, JSON.stringify(progressionTabs[i]));
   }
@@ -205,99 +211,93 @@ function saveAllDataToLocalStorage() {
 //функция удаляет одно todo
 trelloContainerElement.addEventListener('click', deleteOneTodo);
 
-function deleteOneTodo({target}) {
-  if (
-    target.id === 'deleteOneTodoButton' ||
-    target.id === 'deleteOneTodoIcon' ||
-    target.classList.contains('delete-task-button')
-  ) {
-    const cardId = target.closest('.card').id;
-    for (let i = 0; i < progressionTabs.length; i++) {
-      deleteOneTodoHelper(cardId, progressionTabs[i]);
-      saveDataToLocalStorage(localStorageKeys[i], progressionTabs[i]);
-    }
-  }
-}
+// function deleteOneTodo({target}) {
+//   if (
+//     target.id === 'deleteOneTodoButton' ||
+//     target.id === 'deleteOneTodoIcon' ||
+//     target.classList.contains('delete-task-button')
+//   ) {
+//     const cardId = target.closest('.card').id;
+//     for (let i = 0; i < progressionTabs.length; i++) {
+//       deleteOneTodoHelper(cardId, progressionTabs[i]);
+//       saveDataToLocalStorage(localStorageKeys[i], progressionTabs[i]);
+//     }
+//   }
+// }
 
-function deleteOneTodoHelper(cardId, array) {
-  for (let i = 0; i < array.length; i++) {
-    // console.log(typeof cardId, typeof todos[i].id);
-    if (cardId === `${array[i].id}`) {
-      // console.log(todos);
-      array.splice(i, 1);
-      // console.log(todos);
-      renderAll();
-    }
-  }
-}
+// function deleteOneTodoHelper(cardId, array) {
+//   for (let i = 0; i < array.length; i++) {
+//     if (cardId === `${array[i].id}`) {
+//       array.splice(i, 1);
+//       renderAll();
+//     }
+//   }
+// }
 
 //редактирование todo
 const editFormElement = document.querySelector('#todoFormEdit');
 trelloContainerElement.addEventListener('click', handleClickEditTodoForm);
-let saveId;
-function handleClickEditTodoForm({target}) {
-  if (target.classList.contains('edit-todo')) {
-    const cardId = target.closest('.card').id;
-    saveId = cardId;
-    // console.log(saveId, '----------------');
-    for (let i = 0; i < progressionTabs.length; i++) {
-      editTodoFormHelper(cardId, progressionTabs[i]);
-    }
-  }
-}
+// let saveId;
+// function handleClickEditTodoForm({target}) {
+//   if (target.classList.contains('edit-todo')) {
+//     const cardId = target.closest('.card').id;
+//     saveId = cardId;
+//     for (let i = 0; i < progressionTabs.length; i++) {
+//       editTodoFormHelper(cardId, progressionTabs[i]);
+//     }
+//   }
+// }
 
-function editTodoFormHelper(cardId, array) {
-  for (let i = 0; i < array.length; i++) {
-    if (cardId === `${array[i].id}`) {
-      document.getElementById('titleEdit').value = array[i].title;
-      document.getElementById('descriptionEdit').value = array[i].description;
-      document.querySelector('.select-edit-users').value = array[i].user;
-    }
-  }
-}
+// function editTodoFormHelper(cardId, array) {
+//   for (let i = 0; i < array.length; i++) {
+//     if (cardId === `${array[i].id}`) {
+//       document.getElementById('titleEdit').value = array[i].title;
+//       document.getElementById('descriptionEdit').value = array[i].description;
+//       document.querySelector('.select-edit-users').value = array[i].user;
+//     }
+//   }
+// }
 
 //записывание сохраненного результата в таск
 editFormElement.addEventListener('click', handleClickEditTodo);
-function handleClickEditTodo(event) {
-  if (event.target.id === 'confirmEdit') {
-    const cardStatus = document.getElementById(`${saveId}`);
-    switch (cardStatus.parentElement) {
-      case cardContainerInProgress:
-        writeEditedValue(inprogress);
-        saveAllDataToLocalStorage();
-        renderAll();
-        break;
-      case cardContainer:
-        writeEditedValue(todos);
-        saveAllDataToLocalStorage();
-        renderAll();
-        break;
-      case cardContainerDone:
-        writeEditedValue(done);
-        saveAllDataToLocalStorage();
-        renderAll();
-        break;
-      default:
-        break;
-    }
-  }
-}
-
-function writeEditedValue(list) {
-  for (let i = 0; i < list.length; i++) {
-    if (saveId === `${list[i].id}`) {
-      list[i].title = document.getElementById('titleEdit').value;
-      list[i].description = document.getElementById('descriptionEdit').value;
-      list[i].user = document.querySelector('.select-edit-users').value;
-    }
-  }
-}
+// function handleClickEditTodo(event) {
+//   if (event.target.id === 'confirmEdit') {
+//     const cardStatus = document.getElementById(`${saveId}`);
+//     switch (cardStatus.parentElement) {
+//       case cardContainerInProgress:
+//         writeEditedValue(inprogress);
+//         saveAllDataToLocalStorage();
+//         renderAll();
+//         break;
+//       case cardContainer:
+//         writeEditedValue(todos);
+//         saveAllDataToLocalStorage();
+//         renderAll();
+//         break;
+//       case cardContainerDone:
+//         writeEditedValue(done);
+//         saveAllDataToLocalStorage();
+//         renderAll();
+//         break;
+//       default:
+//         break;
+//     }
+//   }
+// }
+// function writeEditedValue(list) {
+//   for (let i = 0; i < list.length; i++) {
+//     if (saveId === `${list[i].id}`) {
+//       list[i].title = document.getElementById('titleEdit').value;
+//       list[i].description = document.getElementById('descriptionEdit').value;
+//       list[i].user = document.querySelector('.select-edit-users').value;
+//     }
+//   }
+// }
 
 //пользователи
 async function getUsers() {
   const users = await fetch('https://jsonplaceholder.typicode.com/users');
   const usersList = await users.json();
-  // console.log(usersList);
   addUsers(usersList);
   editUser(usersList);
 }
@@ -305,14 +305,12 @@ getUsers();
 
 const selectElementAddTodo = document.querySelector('.select-add-users');
 const selectElementEditTodo = document.querySelector('.select-edit-users');
-// console.log(selectElementEditTodo);
 
 function addUsers(usersList) {
   for (let i = 0; i < usersList.length; i++) {
     const newUser = document.createElement('option');
     newUser.value = usersList[i].username;
     newUser.innerHTML = usersList[i].username;
-    // console.log(newUser);
     selectElementAddTodo.append(newUser);
   }
 }
@@ -322,7 +320,6 @@ function editUser(usersList) {
     const newUser = document.createElement('option');
     newUser.value = usersList[i].username;
     newUser.innerHTML = usersList[i].username;
-    // console.log(newUser);
     selectElementEditTodo.append(newUser);
   }
 }
@@ -330,51 +327,51 @@ function editUser(usersList) {
 //перемещение карточек
 trelloContainerElement.addEventListener('click', handleClickChangePanel);
 
-function handleClickChangePanel(event) {
-  if (event.target.id === 'selectPanel') {
-    const cardId = event.target.closest('.card').id;
-    if (event.target.value === 'inprogress') {
-      if (inProgressCounter == 6) {
-        alert('слишком много in progress');
-        renderTodoInProgress();
-      } else {
-        const from = [done, todos];
-        changePanelFunction(from, inprogress, cardId);
-      }
-    }
-    if (event.target.value === 'todo') {
-      const from = [inprogress, done];
-      changePanelFunction(from, todos, cardId);
-    }
-    if (event.target.value === 'done') {
-      const from = [inprogress, todos];
-      changePanelFunction(from, done, cardId);
-    }
-  }
-}
+// function handleClickChangePanel(event) {
+//   if (event.target.id === 'selectPanel') {
+//     const cardId = event.target.closest('.card').id;
+//     if (event.target.value === 'inprogress') {
+//       if (inProgressCounter == 6) {
+//         alert('слишком много in progress');
+//         renderTodoInProgress();
+//       } else {
+//         const from = [done, todos];
+//         changePanelFunction(from, inprogress, cardId);
+//       }
+//     }
+//     if (event.target.value === 'todo') {
+//       const from = [inprogress, done];
+//       changePanelFunction(from, todos, cardId);
+//     }
+//     if (event.target.value === 'done') {
+//       const from = [inprogress, todos];
+//       changePanelFunction(from, done, cardId);
+//     }
+//   }
+// }
 //from это массив из колонок откуда может быть перенесена таска
-function changePanelFunction(from, to, cardId) {
-  for (let i = 0; i < from.length; i++) {
-    for (let j = 0; j < from[i].length; j++) {
-      if (`${from[i][j].id}` === cardId) {
-        to.push(from[i][j]);
-        from[i].splice(j, 1);
-        saveAllDataToLocalStorage();
-        renderAll();
-      }
-    }
-  }
-}
+// function changePanelFunction(from, to, cardId) {
+//   for (let i = 0; i < from.length; i++) {
+//     for (let j = 0; j < from[i].length; j++) {
+//       if (`${from[i][j].id}` === cardId) {
+//         to.push(from[i][j]);
+//         from[i].splice(j, 1);
+//         saveAllDataToLocalStorage();
+//         renderAll();
+//       }
+//     }
+//   }
+// }
 
 //удалить все сделанные
 const deleteAllElement = document.querySelector('.trello__delete-all');
 deleteAllElement.addEventListener('click', handleClickDeleteAll);
 
-function handleClickDeleteAll() {
-  const warning = confirm('delete all tasks from done list?');
-  if (warning) {
-    done.splice(0, done.length);
-    saveDataToLocalStorage('doneList', done);
-    renderTodoDone();
-  }
-}
+// function handleClickDeleteAll() {
+//   const warning = confirm('delete all tasks from done list?');
+//   if (warning) {
+//     done.splice(0, done.length);
+//     saveDataToLocalStorage('doneList', done);
+//     renderTodoDone();
+//   }
+// }
---
+++ src/js/handlers.js
@@ -0,0 +1,155 @@
+import {
+  renderAll,
+  renderTodoInProgress,
+  renderTodoDone,
+  saveAllDataToLocalStorage,
+  saveDataToLocalStorage,
+  addTodo,
+  todos,
+  inprogress,
+  done,
+  progressionTabs,
+  localStorageKeys,
+  inProgressCounter,
+  cardContainerInProgress,
+  cardContainer,
+  cardContainerDone,
+  renderTodo,
+} from './app.js';
+
+//добавление todo после нажатия кнопки confirm
+export function handleClickTodoForm(event) {
+  if (event.target.id === 'confirm') {
+    addTodo();
+  }
+}
+
+//функция удаляет одно todo
+export function deleteOneTodo({target}) {
+  if (
+    target.id === 'deleteOneTodoButton' ||
+    target.id === 'deleteOneTodoIcon' ||
+    target.classList.contains('delete-task-button')
+  ) {
+    const cardId = target.closest('.card').id;
+    for (let i = 0; i < progressionTabs.length; i++) {
+      deleteOneTodoHelper(cardId, progressionTabs[i]);
+      saveDataToLocalStorage(localStorageKeys[i], progressionTabs[i]);
+    }
+  }
+}
+
+function deleteOneTodoHelper(cardId, array) {
+  for (let i = 0; i < array.length; i++) {
+    if (cardId === `${array[i].id}`) {
+      array.splice(i, 1);
+
+      renderAll();
+    }
+  }
+}
+
+//редактирование todo
+let saveId;
+export function handleClickEditTodoForm({target}) {
+  if (target.classList.contains('edit-todo')) {
+    const cardId = target.closest('.card').id;
+    saveId = cardId;
+    for (let i = 0; i < progressionTabs.length; i++) {
+      editTodoFormHelper(cardId, progressionTabs[i]);
+    }
+  }
+}
+
+function editTodoFormHelper(cardId, array) {
+  for (let i = 0; i < array.length; i++) {
+    if (cardId === `${array[i].id}`) {
+      document.getElementById('titleEdit').value = array[i].title;
+      document.getElementById('descriptionEdit').value = array[i].description;
+      document.querySelector('.select-edit-users').value = array[i].user;
+    }
+  }
+}
+
+//записывание сохраненного результата в таск
+export function handleClickEditTodo(event) {
+  if (event.target.id === 'confirmEdit') {
+    const cardStatus = document.getElementById(`${saveId}`);
+    switch (cardStatus.parentElement) {
+      case cardContainerInProgress:
+        writeEditedValue(inprogress);
+        saveAllDataToLocalStorage();
+        renderAll();
+        break;
+      case cardContainer:
+        writeEditedValue(todos);
+        saveAllDataToLocalStorage();
+        renderAll();
+        break;
+      case cardContainerDone:
+        writeEditedValue(done);
+        saveAllDataToLocalStorage();
+        renderAll();
+        break;
+      default:
+        break;
+    }
+  }
+}
+function writeEditedValue(list) {
+  for (let i = 0; i < list.length; i++) {
+    if (saveId === `${list[i].id}`) {
+      list[i].title = document.getElementById('titleEdit').value;
+      list[i].description = document.getElementById('descriptionEdit').value;
+      list[i].user = document.querySelector('.select-edit-users').value;
+    }
+  }
+}
+
+//перемещение карточек
+export function handleClickChangePanel(event) {
+  if (event.target.id === 'selectPanel') {
+    const cardId = event.target.closest('.card').id;
+    if (event.target.value === 'inprogress') {
+      if (+inProgressCounter.innerHTML != 6) {
+        const from = [done, todos];
+        changePanelFunction(from, inprogress, cardId);
+      } else {
+        alert('слишком много in progress');
+        renderTodo();
+        renderTodoDone();
+      }
+    }
+    if (event.target.value === 'todo') {
+      const from = [inprogress, done];
+      changePanelFunction(from, todos, cardId);
+    }
+    if (event.target.value === 'done') {
+      const from = [inprogress, todos];
+      changePanelFunction(from, done, cardId);
+    }
+  }
+}
+//from это массив из колонок откуда может быть перенесена таска
+function changePanelFunction(from, to, cardId) {
+  for (let i = 0; i < from.length; i++) {
+    for (let j = 0; j < from[i].length; j++) {
+      if (`${from[i][j].id}` === cardId) {
+        to.push(from[i][j]);
+        from[i].splice(j, 1);
+        saveAllDataToLocalStorage();
+        renderAll();
+      }
+    }
+  }
+}
+
+//удалить все сделанные
+export function handleClickDeleteAll() {
+  const warning = confirm('delete all tasks from done list?');
+  if (warning) {
+    done.splice(0, done.length);
+    saveDataToLocalStorage('doneList', done);
+    renderTodoDone();
+  }
+}
